6806907823e505815d65c362e7b1e46599b23e75
Gahen <gahen.2007@gmail.com>
2024-08-29 17:53:24 -0300

    Segundo Commit

--- FIAPTokio/src/logica/aula06/exercicios/Exercicio09.java
+++ FIAPTokio/src/logica/aula06/exercicios/Exercicio09.java
@@ -1,5 +1,21 @@
 package logica.aula06.exercicios;
-
+import java.util.Scanner;
 public class Exercicio09 {
-
+public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.printf("Qual o seu tipo de conta: \n\n[A] Residencial\n[B] Comercial\n[C] Industrial\n\n");
+		String tipo = sc.nextLine();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
